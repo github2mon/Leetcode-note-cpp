37ad39c581c6a8dccec011b19123f1317672b793
Wang Yuming <44386103+github2mon@users.noreply.github.com>
2019-12-08 23:48:32 +0800

    Update 010_regular-expression-matching.cpp

--- 010_regular-expression-matching.cpp
+++ 010_regular-expression-matching.cpp
@@ -9,40 +9,6 @@ The matching should cover the entire input string (not partial).
 Note:
 s could be empty and contains only lowercase letters a-z.
 p could be empty and contains only lowercase letters a-z, and characters like . or *.
-
-Example 1:
-Input:
-s = "aa"
-p = "a"
-Output: false
-Explanation: "a" does not match the entire string "aa".
-
-Example 2:
-Input:
-s = "aa"
-p = "a*"
-Output: true
-Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
-
-Example 3:
-Input:
-s = "ab"
-p = ".*"
-Output: true
-Explanation: ".*" means "zero or more (*) of any character (.)".
-
-Example 4:
-Input:
-s = "aab"
-p = "c*a*b"
-Output: true
-Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
-
-Example 5:
-Input:
-s = "mississippi"
-p = "mis*is*p*."
-Output: false
 */
 
 class Solution {
@@ -93,3 +59,21 @@ public:
 		return v[m][n];
 	}
 };
+
+int main()
+{
+	Solution *s = new Solution;
+	cout << (s->isMatch("", "a") == false) << endl;
+	cout << (s->isMatch("", "a*c*d*") == true) << endl;
+	cout << (s->isMatch("aa", "aab") == false) << endl;
+	cout << (s->isMatch("aa", "aab*c") == false) << endl;
+	cout << (s->isMatch("aa", "a*") == true) << endl;
+	cout << (s->isMatch("ab", ".*") == true) << endl;
+	cout << (s->isMatch("aab", "ca*b*") == false) << endl;
+	cout << (s->isMatch("aaa", "a*a") == true) << endl;
+	cout << (s->isMatch("aaa", "a*c*d*a") == true) << endl;
+	cout << (s->isMatch("aaa", "a*c*d*ab") == false) << endl;
+	cout << (s->isMatch("aaa", "a*c*d*") == true) << endl;
+	cout << (s->isMatch("aaa", "ab*a") == false) << endl;
+	return 0;
+}

baa841935b7a0d7871608ddc4d1902cde1ac84c9
Wang Yuming <44386103+github2mon@users.noreply.github.com>
2019-12-08 23:40:08 +0800

    Update and rename 010_ to 010_regular-expression-matching.cpp

--- 010_regular-expression-matching.cpp
+++ 010_regular-expression-matching.cpp
@@ -51,7 +51,7 @@ public:
 		if (p.empty())
 			return s.empty();
 
-		if (p[1] == '*')
+		if (p[1] == '*') //"x*" repeats any time: isMatch(s, p.substr(2))
 			return isMatch(s, p.substr(2)) || !s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p);
 		else
 			return !s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p.substr(1));
